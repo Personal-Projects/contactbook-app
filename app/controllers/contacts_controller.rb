604c2680d98bf1bae6c85dc47293a460e69338e1
jasmine <jasliu2012@gmail.com>
2017-08-19 19:05:35 -0500

    made show page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -21,6 +21,7 @@ class ContactsController < ApplicationController
       latitude: input_latitude,
       longitude: input_longitude
     )
+    flash[:success] = "Student added!"
     redirect_to "/contacts"
   end
 
@@ -34,10 +35,22 @@ class ContactsController < ApplicationController
   end
 
   def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.update(
+      first_name: params[:first_name],
+      middle_name: params[:middle_name],
+      last_name: params[:last_name],
+      number: params[:number],
+      email: params[:email],
+      bio: params[:bio]
+    )
     redirect_to "/contacts"
   end
 
   def destroy
+    @contact = Contact.find_by(id: params[:id])
+    @contact.destroy
+    flash[:warning] = "Contact deleted."
     redirect_to "/contacts"
   end
 end
